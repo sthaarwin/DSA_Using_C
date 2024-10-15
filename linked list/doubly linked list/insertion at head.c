ed61784c7729e59cd0ef6587e97760914b21603b
sthaarwin <sthaarwin123@gmail.com>
2024-10-15 11:14:09 +0545

    ok.

---
+++ linked list/doubly linked list/insertion at head.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+    int data;
+    struct node* prev;
+    struct node* next;
+};
+
+struct node* head = NULL;
+
+void insert(int value){
+    struct node* temp = (struct node*)malloc(sizeof(struct node));
+    temp -> data = value;
+    temp -> next = NULL;
+
+    if(head == NULL){
+        head = temp;
+        return;
+    }
+
+    head -> prev = temp;
+    temp -> next = head;
+    head = temp;
+}
+
+void print(){
+    struct node* temp = head;
+    while(temp != NULL){
+        printf("%d -> ", temp -> data);
+        temp = temp -> next;
+    }
+    printf("null\n");
+}
+
+int main(){
+    print();
+    insert(6);
+    insert(2);
+    print();
+    insert(3);
+    insert(5);
+    insert(7);
+    print();
+    return 0;
+}
