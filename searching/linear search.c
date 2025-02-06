c1ecb9a5615143cf45df5405bf1bd92d1f2adb49
Axzyte <sthaarwin123@gmail.com>
2025-02-06 12:49:30 +0545

    all these searching algorithms yet can't search better future

---
+++ searching/linear search.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int linearSearch(int arr[], int size, int target) {
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == target) {
+            return i; // Return the index of the target element
+        }
+    }
+    return -1; // Return -1 if the target element is not found
+}
+
+int main() {
+    int arr[] = {2, 4, 6, 8, 10, 12, 14};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int target = 10;
+    
+    int result = linearSearch(arr, size, target);
+    
+    if (result != -1) {
+        printf("Element found at index %d\n", result);
+    } else {
+        printf("Element not found in the array\n");
+    }
+    
+    return 0;
+}
